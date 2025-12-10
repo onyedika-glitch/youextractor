7e9a12cfb5440218cc1b69048f0df1d54b16da65
Omogo Peter <omogopeter48@gmail.com>
2025-12-10 02:37:38 +0100

    Add new files

--- youextractor/routes/api.php
+++ youextractor/routes/api.php
@@ -11,7 +11,7 @@ Route::get('/health', function () {
     ]);
 });
 
-Route::get('/videos', [VideoController::class, 'index']);
-Route::get('/videos/search', [VideoController::class, 'search']);
 Route::post('/videos/extract', [VideoController::class, 'extract']);
-Route::get('/videos/{video}', [VideoController::class, 'show']);
+Route::get('/videos', [VideoController::class, 'index']);
+Route::get('/videos/search', [VideoController::class, 'search']);  // static route
+Route::get('/videos/{video}', [VideoController::class, 'show']);   // dynamic route last
