f436ef096fddf286f1911697dfea183f92dee5d4
onyedika-glitch <omogopeter48@gmail.com>
2026-06-07 11:41:45 +0000

    tracking

--- youextractor/resources/views/auth/signup.blade.php
+++ youextractor/resources/views/auth/signup.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script async src="https://aromatic-caribou-889.convex.site/api/a/am_qYeSPvXGoob8W5b-"></script>
     <title>Sign Up - YouTube Code Extractor</title>
     
     <!-- Design System CSS -->
